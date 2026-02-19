75d3ddcf7921e3dda3cdb0215e070405cfbaed14
deceive0112 <94655133+deceive0112@users.noreply.github.com>
2026-02-20 02:01:11 +0800

    fix: minor updates

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -12,20 +12,29 @@ const isHovered = ref(false)
     <div class="basis-3/5">
       <h1 class="text-4xl mt-20 font-bold backdrop-blur-lg rounded-xl">Hey! I'm 
         <a class="text-blue-500 cursor-pointer"  
-           @mouseenter="isHovered = true"
+          @mouseenter="isHovered = true"
           @mouseleave="isHovered = false">{{ title }}</a>
       </h1> 
-      <p class="text-xl mt-5 mb-50 backdrop-blur-lg rounded-xl">
+      <p class="text-xl mt-5 backdrop-blur-lg rounded-xl">
         Flexbox is ideal for one-dimensional (linear) layouts, arranging items in a single row or column, while Grid is designed for two-dimensional (grid-based) layouts, 
         managing both rows and columns simultaneously. Use Flexbox for component-level alignment and Grid for overall page structure. 
       </p>
+      <div class="flex flex-row mt-5 mb-25 gap-3 backdrop-blur-lg rounded-xl">
+        <UIcon name="mdi:github" class="size-6"/>
+        <span class="text-lg font-bold font-mono">Github</span>
+        <span class="text-lg font-bold font-mono">|</span>
+        <UIcon name="mdi:linkedin" class="size-6"/>
+        <span class="text-lg font-bold font-mono">LinkedIn</span>
+        <span class="text-lg font-bold font-mono">|</span>
+        <span class="text-lg font-bold font-mono px-3">More about me... -></span>
+      </div>
     </div>
-    <div class="flex basis-1/5 items-center justify-center">
+    <div class="flex basis-1/5 items-center justify-center mt-20">
       <div class="backdrop-blur-lg rounded-xl p-3 mb-25">
       <img src="/gradpicprof2.jpg" class="flex center w-50 h-50 rounded-lg shadow-lg" />
       </div>
     </div>
-    <div class="flex basis-1/5 items-center justify-center">
+    <div class="flex basis-1/5 items-center justify-center mt-20">
       <Transition
         enter-active-class="transition-all duration-500 delay-300"
         enter-from-class="opacity-0 -translate-x-10"
@@ -41,17 +50,7 @@ const isHovered = ref(false)
     </div>
   </div>
 
-  <div class="flex items-center justify-center">
-    <svg xmlns="http://www.w3.org/2000/svg" width="600" height="600" viewBox="0 0 24 24">
-      <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
-        <path
-          d="m8.005 20.005l.011-.01m-4.011.01l.011-.01m-.011-3.99l.011-.01m-.011-3.99l.011-.01m-.011-3.99l.011-.01m-.011-3.99l.011-.01m3.989.01l.011-.01" />
-        <path fill="currentColor" d="M12.005 20.005h8v-16h-8z" />
-      </g>
-    </svg>
-  </div>
-
-  <div class="grid grid-cols-3 gap-4">
+  <div class="grid grid-cols-3 gap-4 backdrop-blur-3xl rounded-xl">
     <div class="flex items-center text-center justify-center col-span-2 p-4">
       01
     </div>
