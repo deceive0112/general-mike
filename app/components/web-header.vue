a281ad81dfe796e3a18ee22f9535a758127942ca
deceive0112 <94655133+deceive0112@users.noreply.github.com>
2026-02-19 14:45:09 +0800

    feat: add dark/light theme mode

--- app/components/web-header.vue
+++ app/components/web-header.vue
@@ -1,5 +1,6 @@
 <template> 
-    <div class="sticky top-0 left-0 w-full z-50 bg-transparent text-center backdrop-blur-sm">
-    <h1 class="text-white text-2xl font-bold">Header</h1>
+    <div class="flex flex-row justify-between sticky top-0 left-0 w-full z-50 bg-transparent items-center text-center backdrop-blur-sm">
+    <h1 class="text-2xl font-bold">Header</h1>
+    <UColorModeButton />
     </div>
 </template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,9 @@
 <template> 
-    <div class="flex flex-col min-h-screen">
-        <web-header class="bg-blue-900 p-3 shadow-md"/>
+    <div class="flex flex-col min-h-screen px-18 md:px-30">
+        <web-header class="bg-blue-900 p-3 shadow-xl rounded-md mt-2"/>
         <div class="p-3">
             <slot />
         </div>
-        <web-footer class="bg-blue-900 p-3 shadow-md"/>
+        <web-footer class="bg-blue-900 p-3 shadow-xl"/>
     </div>
 </template>
